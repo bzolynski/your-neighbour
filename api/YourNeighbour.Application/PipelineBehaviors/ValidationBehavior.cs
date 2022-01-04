2a12ee203045656248f04437ceb6452a1b625526
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-01-04 11:55:22 +0100

    Add validation behavior

---
+++ api/YourNeighbour.Application/PipelineBehaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace YourNeighbour.Application.PipelineBehaviors
+{
+    public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            this.validators = validators;
+        }
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (!validators.Any())
+                return await next();
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var errors = validators.Select(x => x.Validate(context))
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .ToList();
+
+            if (errors.Any())
+                throw new ValidationException(errors);
+
+            return await next();
+        }
+    }
+}
