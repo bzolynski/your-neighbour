f7a763cacbd6407965ac2f8f6e78d5b4e1529021
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-01-04 12:06:10 +0100

    Routing settings in app.module

--- client-app/src/app/app-routing.module.ts
+++ client-app/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		component: HomeComponent
+	},
+	{
+		path: 'settings',
+		loadChildren: () =>
+			import('./modules/settings/settings.module').then((m) => m.SettingsModule)
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './modules/core/core.module';
+import { HomeModule } from './modules/home/home.module';
+import { SettingsModule } from './modules/settings/settings.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [
+		AppRoutingModule,
+		BrowserModule,
+		CoreModule,
+		HomeModule,
+		RouterModule,
+		SharedModule,
+		SettingsModule
+	],
+	bootstrap: [ AppComponent ],
+	exports: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
