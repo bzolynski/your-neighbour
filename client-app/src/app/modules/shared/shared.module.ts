c2e57df401062a969e98befbe6a3735938049cea
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-01-15 23:19:26 +0100

    Update Shared module to use Angular Material

--- client-app/src/app/modules/shared/shared.module.ts
+++ client-app/src/app/modules/shared/shared.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TextInputWithDropdownComponent } from './components/form/text-input-with-dropdown/text-input-with-dropdown.component';
 import { RouterModule } from '@angular/router';
-import { SpinnerButtonComponent } from './components/buttons/spinner-button/spinner-button.component';
 import { TextInputComponent } from './components/inputs/text-input/text-input.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [
-        TextInputWithDropdownComponent,
-        SpinnerButtonComponent,
-        TextInputComponent,
-    ],
+    declarations: [TextInputComponent],
     imports: [
         CommonModule,
         RouterModule,
         AngularMaterialModule,
         ReactiveFormsModule,
     ],
-    exports: [
-        TextInputWithDropdownComponent,
-        SpinnerButtonComponent,
-        TextInputComponent,
-    ],
+    exports: [TextInputComponent],
 })
 export class SharedModule {}
