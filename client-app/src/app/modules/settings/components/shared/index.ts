e21b479ad42ad8967e790ee0ccba5d60d3830afc
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-01-15 12:32:28 +0100

    Moved shared setting components to Shared folder. Changed Settings Layout component

---
+++ client-app/src/app/modules/settings/components/shared/index.ts
@@ -0,0 +1,3 @@
+export * from './settings-layout/settings-layout.component';
+export * from './settings-side-bar/settings-side-bar.component';
+export * from './settings-side-bar-group/settings-side-bar-group.component';
---
+++ client-app/src/app/modules/settings/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './category-definition-settings/category-definition-settings.component';
+export * from './category-settings/category-settings.component';
+export * from './settings/settings.component';
