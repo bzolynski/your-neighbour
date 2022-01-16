f695e051f96ab0e51216079869ca32348023141b
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-01-16 12:34:54 +0100

    Change welcome login component

--- client-app/src/app/modules/home/components/welcome-login-form/welcome-login-form.component.ts
+++ client-app/src/app/modules/home/components/welcome-login-form/welcome-login-form.component.ts
@@ -1,13 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-welcome-login-form',
     templateUrl: './welcome-login-form.component.html',
     styleUrls: ['./welcome-login-form.component.scss'],
 })
-export class WelcomeLoginFormComponent implements OnInit {
-    hide: boolean = true;
-    constructor() {}
+export class WelcomeLoginFormComponent implements OnInit, OnDestroy {
+    // Public properties
+    form: FormGroup = this.fb.group({
+        login: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+    });
+
+    get loginErrorMessage(): string {
+        const loginControl = this.form.controls['login'];
+        if (loginControl.errors?.required) return 'Pole jest wymagane';
+        return '';
+    }
+    get passwordErrorMessage(): string {
+        const passwordControl = this.form.controls['password'];
+        if (passwordControl.errors?.required) return 'Pole jest wymagane';
+        return '';
+    }
+
+    // Private members
+    destroy$: Subject<boolean> = new Subject<boolean>();
+
+    constructor(private fb: FormBuilder) {}
 
     ngOnInit(): void {}
+
+    ngOnDestroy(): void {
+        this.destroy$.next(true);
+        this.destroy$.unsubscribe;
+    }
+
+    onSubmit = () => {
+        console.log(this.form.value);
+    };
 }
