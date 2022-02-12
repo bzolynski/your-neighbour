0605ac883c89b198daec6707e1f754bc328eef7b
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-02-12 12:55:26 +0100

    Add auth-related buttons to header navbar

--- client-app/src/app/modules/core/header/header.component.ts
+++ client-app/src/app/modules/core/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication/authentication.service';
+import { IUser } from '../models/user.model';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html',
-	styleUrls: [ './header.component.scss' ]
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
+    constructor(private authenticationService: AuthenticationService) {}
+    get user(): IUser | null {
+        const user = localStorage.getItem('user');
+        if (user) return JSON.parse(user);
+        return null;
+    }
+    ngOnInit(): void {}
+    logout = () => {
+        this.authenticationService.logout();
+    };
 }
