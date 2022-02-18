fd9f10d114fc378e39a462d24708dc99a2527d10
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-02-18 22:07:17 +0100

    Add dictionary type

---
+++ client-app/src/app/modules/core/types/dictionary.ts
@@ -0,0 +1,24 @@
+export interface IDictionary<TKey, TValue> extends Map<TKey, TValue> {
+    containsValue: (value: TValue) => boolean;
+    get: (key: TKey) => TValue;
+}
+
+export class Dictionary<TKey, TValue>
+    extends Map<TKey, TValue>
+    implements IDictionary<TKey, TValue>
+{
+    constructor();
+    constructor(dict: Dictionary<TKey, TValue>);
+    constructor(dict?: Dictionary<TKey, TValue>) {
+        super(dict ?? new Map<TKey, TValue>());
+    }
+
+    containsValue = (value: TValue): boolean => {
+        return Array.from(super.values()).findIndex((x) => x == value) > -1;
+    };
+
+    get = (key: TKey): TValue => {
+        if (!super.has(key)) throw new Error('Key missing in dictionary!');
+        return super.get(key)!;
+    };
+}
