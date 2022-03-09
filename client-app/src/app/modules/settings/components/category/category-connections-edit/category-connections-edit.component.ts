90af549066d6930fb24bdcb29985029002a8825b
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-03-09 20:36:44 +0100

    Basic layout for category connections tree view

--- client-app/src/app/modules/settings/components/category/category-connections-edit/category-connections-edit.component.ts
+++ client-app/src/app/modules/settings/components/category/category-connections-edit/category-connections-edit.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ICategory } from 'src/app/modules/core/models';
+import { CategoryService } from 'src/app/modules/core/services';
+import { ITree } from 'src/app/modules/core/types';
 
 @Component({
-  selector: 'app-category-connections-edit',
-  templateUrl: './category-connections-edit.component.html',
-  styleUrls: ['./category-connections-edit.component.scss']
+    selector: 'app-category-connections-edit',
+    templateUrl: './category-connections-edit.component.html',
+    styleUrls: ['./category-connections-edit.component.scss'],
 })
-export class CategoryConnectionsEditComponent implements OnInit {
+export class CategoryConnectionsEditComponent implements OnInit, OnDestroy {
+    treeItem: ITree<ICategory> | undefined;
+    destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
+    constructor(private categoryService: CategoryService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.categoryService
+            .getAll()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((response) => {
+                this.treeItem = response.responseObject
+                    .toLookup(
+                        (x) => x,
+                        (x) => x,
+                        (p, c) => p.id == c.parentId
+                    )
+                    .toTree();
+                console.log('tree item w componencie');
+                console.log(this.treeItem);
+            });
+    }
 
+    ngOnDestroy(): void {
+        this.destroy$.next(true);
+        this.destroy$.unsubscribe();
+    }
 }
