32d5361654e5dacb53452274e81c6fcbb3899e87
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-08 19:48:23 +0200

    Working map

--- client-app/src/app/modules/shared/components/map/map.component.ts
+++ client-app/src/app/modules/shared/components/map/map.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
 import { Feature } from 'src/app/modules/core/models/map-response.model';
+import { ICoordinates } from 'src/app/modules/core/models/localization.model';
 
 @Component({
     selector: 'app-map',
@@ -14,39 +13,46 @@ import { Feature } from 'src/app/modules/core/models/map-response.model';
     styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+    @Input() coordinates?: ICoordinates;
     map!: mapboxgl.Map;
+    geocoder!: MapboxGeocoder;
     marker!: mapboxgl.Marker;
     selectedPoint?: Feature;
 
     ngOnInit(): void {
         this.marker = new mapboxgl.Marker();
         this.initializeMap();
+        this.initializeGeocoder();
+        this.initializeEvents();
     }
+
     initializeMap = () => {
         this.map = new mapboxgl.Map({
             container: 'map',
             zoom: 5,
             style: 'mapbox://styles/mapbox/streets-v11',
             accessToken: environment.mapbox.accessToken,
-            center: [19.227407189927987, 52.06096121526846],
+            //center: new mapboxgl.LngLat(19.22, 52.06),
         });
-        const geocoderControl = new MapboxGeocoder({
+    };
+
+    initializeGeocoder = () => {
+        this.geocoder = new MapboxGeocoder({
             accessToken: environment.mapbox.accessToken,
             marker: false,
             countries: 'pl',
-            // had to ignore bellow line, wrong type expected
-            // @ts-ignore
+            // @ts-ignore: problem with type from @types/mapbox-gl
             mapboxgl: mapboxgl,
             reverseGeocode: true,
         });
+        this.map.addControl(this.geocoder);
+    };
 
-        this.map.addControl(geocoderControl);
-
+    initializeEvents = () => {
         this.map.on('click', (e) => {
-            //geocoderControl.setInput(`${e.lngLat.lat},${e.lngLat.lng}`);
-            geocoderControl.query(`${e.lngLat.lat},${e.lngLat.lng}`);
+            this.geocoder.query(`${e.lngLat.lat},${e.lngLat.lng}`);
         });
-        geocoderControl.on('result', (obj: { result: Feature }) => {
+        this.geocoder.on('result', (obj: { result: Feature }) => {
             this.selectedPoint = obj.result ?? null;
             this.moveMarker(new mapboxgl.LngLat(obj.result.center[0], obj.result.center[1]));
         });
