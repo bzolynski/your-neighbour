b277e7813b66175746aabe05a2ede02e4677248b
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-08 19:46:56 +0200

    Rename localization service

--- client-app/src/app/modules/core/services/localization.service.ts
+++ client-app/src/app/modules/core/services/localization.service.ts
@@ -6,10 +6,14 @@ import { ApiService } from './api.service';
 @Injectable({
     providedIn: 'root',
 })
-export class LocalizationServiceService {
+export class LocalizationService {
     constructor(private apiService: ApiService) {}
 
+    getManyByUser = (userId: number): ObservableResponse<Array<ILocalizationDto>> => {
+        return this.apiService.get<Array<ILocalizationDto>>(`localization/get-many-by-user/${userId}`);
+    };
+
     create = (body: ILocalizationCreateDto): ObservableResponse<ILocalizationDto> => {
-        return this.apiService.put<ILocalizationDto>('localizaiton/create-for-user', body);
+        return this.apiService.put<ILocalizationDto>('localization/create-for-user', body);
     };
 }
