8a9b02671338bd9aff7deb286fa1be10984a8168
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-11 20:44:45 +0200

    Add input prop for backdrop background color

--- client-app/src/app/modules/shared/components/backdrop-loading/backdrop-loading.component.ts
+++ client-app/src/app/modules/shared/components/backdrop-loading/backdrop-loading.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-backdrop-loading',
     templateUrl: './backdrop-loading.component.html',
     styleUrls: ['./backdrop-loading.component.scss'],
 })
-export class BackdropLoadingComponent {}
+export class BackdropLoadingComponent {
+    @Input()
+    backdropColor: string = 'hsl(0, 0%, 100%)';
+}
