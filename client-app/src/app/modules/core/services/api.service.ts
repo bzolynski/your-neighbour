f60ccbfac4fad7daff9d0564c0e5e7c3415030ff
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-28 16:55:18 +0200

    FIx formating

--- client-app/src/app/modules/core/services/api.service.ts
+++ client-app/src/app/modules/core/services/api.service.ts
@@ -12,7 +12,6 @@ import { HttpError } from '../models';
 export class ApiService {
     constructor(private client: HttpClient) {}
 
-    private headers = new Headers().append('Content-Type', 'application/json');
     private handleError = (httpError: HttpError<Response>, caught: Observable<any>) => {
         // for ex: logs
         console.log(httpError);
@@ -21,28 +20,18 @@ export class ApiService {
     };
 
     get = <T>(path: string, params?: HttpParams): ObservableResponse<T> => {
-        return this.client
-            .get<T>(`${environment.api_url}/${path}`, { params: params })
-            .pipe(catchError(this.handleError));
+        return this.client.get<T>(`${environment.api_url}/${path}`, { params: params }).pipe(catchError(this.handleError));
     };
     post = <T>(path: string, body: Object): ObservableResponse<T> => {
-        return this.client
-            .post<T>(`${environment.api_url}/${path}`, body)
-            .pipe(catchError(this.handleError));
+        return this.client.post<T>(`${environment.api_url}/${path}`, body).pipe(catchError(this.handleError));
     };
     patch = <T>(path: string, body?: Object): ObservableResponse<T> => {
-        return this.client
-            .patch<T>(`${environment.api_url}/${path}`, body)
-            .pipe(catchError(this.handleError));
+        return this.client.patch<T>(`${environment.api_url}/${path}`, body).pipe(catchError(this.handleError));
     };
     put = <T>(path: string, body: Object): ObservableResponse<T> => {
-        return this.client
-            .put<T>(`${environment.api_url}/${path}`, body)
-            .pipe(catchError(this.handleError));
+        return this.client.put<T>(`${environment.api_url}/${path}`, body).pipe(catchError(this.handleError));
     };
     delete = <T>(path: string): ObservableResponse<T> => {
-        return this.client
-            .delete<T>(`${environment.api_url}/${path}`)
-            .pipe(catchError(this.handleError));
+        return this.client.delete<T>(`${environment.api_url}/${path}`).pipe(catchError(this.handleError));
     };
 }
