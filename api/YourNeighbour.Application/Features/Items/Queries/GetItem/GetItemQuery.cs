d1280ff3a22030148aba9400a7c0afafa6564f96
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-28 16:46:20 +0200

    Add get item feature with query params

---
+++ api/YourNeighbour.Application/Features/Items/Queries/GetItem/GetItemQuery.cs
@@ -0,0 +1,7 @@
+﻿using YourNeighbour.Application.Abstractions;
+using YourNeighbour.Application.Features.Items.Dtos;
+
+namespace YourNeighbour.Application.Features.Items.Queries.GetItem
+{
+    public sealed record GetItemQuery(int Id, ItemQueryParams QueryParams) : IQuery<ItemDto>;
+}
---
+++ api/YourNeighbour.Application/Features/Items/Queries/ItemQueryParams.cs
@@ -0,0 +1,10 @@
+﻿namespace YourNeighbour.Application.Features.Items.Queries
+{
+    public sealed class ItemQueryParams
+    {
+        public bool IncludeCategory { get; set; }
+        public bool IncludeUser { get; set; }
+        public bool IncludeImages { get; set; }
+        public int? MaxImages { get; set; }
+    }
+}
