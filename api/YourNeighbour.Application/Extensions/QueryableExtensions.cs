1917be875cdcf100cd3b090d8c714f9a18ec427b
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-28 16:45:22 +0200

    Add include if extension

--- api/YourNeighbour.Application/Extensions/QueryableExtensions.cs
+++ api/YourNeighbour.Application/Extensions/QueryableExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using YourNeighbour.Application.Abstractions;
 
 namespace YourNeighbour.Application.Extensions
@@ -8,6 +11,12 @@ namespace YourNeighbour.Application.Extensions
         public static IQueryable<T> ProjectTo<T>(this IQueryable source, IMapper mapper) where T : class
         {
             return mapper.ProjectTo<T>(source);
+        }
+        public static IQueryable<TEntity> IncludeIf<TEntity, TProperty>(this IQueryable<TEntity> source, Expression<Func<TEntity, TProperty>> navigationPropertyPath, bool condition) where TEntity : class
+        {
+            if (condition)
+                return source.Include(navigationPropertyPath);
+            return source;
         }
     }
 }
