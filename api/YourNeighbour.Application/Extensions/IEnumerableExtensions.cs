94166543071c162c097001222f8184d43aff036e
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-28 16:45:33 +0200

    Add TakeNullable extension

---
+++ api/YourNeighbour.Application/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace YourNeighbour.Application.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> TakeNullable<T>(this IEnumerable<T> source, int? quantity)
+        {
+            if (quantity.HasValue)
+                return source.Take(quantity.Value);
+            return source;
+        }
+    }
+}
