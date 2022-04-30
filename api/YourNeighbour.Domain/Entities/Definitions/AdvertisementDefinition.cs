fe7018ec6d948bcd7e28c36b51493ad8c7df6928
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-04-30 14:39:38 +0200

    Add Basic property to advertisement | Add root guid | Add migration

--- api/YourNeighbour.Domain/Entities/Definitions/AdvertisementDefinition.cs
+++ api/YourNeighbour.Domain/Entities/Definitions/AdvertisementDefinition.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace YourNeighbour.Domain.Entities.Definitions
 {
     public class AdvertisementDefinition : DefinitionBase
     {
+        public static readonly Guid RootAdvertisementDefinitionGuid = new Guid("00000000-0000-0000-0000-000000000000");
+
         public AdvertisementDefinition()
         {
             Advertisements = new HashSet<Advertisement>();
         }
         public ICollection<Advertisement> Advertisements { get; private set; }
+        public bool Basic { get; set; } = false;
     }
 }
