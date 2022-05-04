4b79c18fda27416ab75d6bdc2f56906c3fa42ec4
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-04 22:01:26 +0200

    Add categories select to side panel

--- client-app/src/app/advertisements/ui/list-side-panel/list-side-panel.component.ts
+++ client-app/src/app/advertisements/ui/list-side-panel/list-side-panel.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ICategory } from 'src/app/shared/data-access/models';
 
 @Component({
     selector: 'app-list-side-panel',
     templateUrl: './list-side-panel.component.html',
     styleUrls: ['./list-side-panel.component.scss'],
 })
-export class ListSidePanelComponent {}
+export class ListSidePanelComponent {
+    @Input() category?: ICategory | null;
+}
--- client-app/src/app/advertisements/ui/list-side-panel/list-side-panel.module.ts
+++ client-app/src/app/advertisements/ui/list-side-panel/list-side-panel.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListSidePanelComponent } from './list-side-panel.component';
+import { SkeletonTextModule } from 'src/app/shared/ui/skeleton-text/skeleton-text.module';
+import { RouterModule } from '@angular/router';
+import { RepeatModule } from 'src/app/shared/directives/repeat/repeat.module';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SkeletonTextModule, RouterModule, RepeatModule],
     declarations: [ListSidePanelComponent],
     exports: [ListSidePanelComponent],
 })
