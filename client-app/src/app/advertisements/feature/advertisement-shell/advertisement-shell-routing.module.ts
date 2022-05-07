271e238775930c0b0e2f0bd0dc338f36d848cbc1
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-07 18:57:48 +0200

    Fix problem with routing

--- client-app/src/app/advertisements/feature/advertisement-shell/advertisement-shell-routing.module.ts
+++ client-app/src/app/advertisements/feature/advertisement-shell/advertisement-shell-routing.module.ts
@@ -3,11 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: ':id',
-        pathMatch: 'full',
-        loadChildren: () => import('../advertisement-list/advertisement-list.module').then((m) => m.AdvertisementListModule),
-    },
-    {
         path: 'add',
         pathMatch: 'full',
         loadChildren: () => import('../advertisement-add/advertisement-add.module').then((m) => m.AdvertisementAddModule),
@@ -15,6 +10,11 @@ const routes: Routes = [
     {
         path: 'details/:id',
         loadChildren: () => import('../advertisement/advertisement.module').then((m) => m.AdvertisementModule),
+    },
+    {
+        path: ':id',
+        pathMatch: 'full',
+        loadChildren: () => import('../advertisement-list/advertisement-list.module').then((m) => m.AdvertisementListModule),
     },
 ];
 
