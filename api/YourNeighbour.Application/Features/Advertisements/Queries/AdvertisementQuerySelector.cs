c8886f6609b45d49a4fe8141fe3af895320e5a20
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-07 21:47:15 +0200

    Add QuerySelector extension methods for Advertisement

---
+++ api/YourNeighbour.Application/Features/Advertisements/Queries/AdvertisementQuerySelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using YourNeighbour.Application.Extensions;
+using YourNeighbour.Domain.Entities;
+
+namespace YourNeighbour.Application.Features.Advertisements.Queries
+{
+    public static class AdvertisementQuerySelector
+    {
+        public static IQueryable<Advertisement> ApplySearchableQueryParams(this IQueryable<Advertisement> query, AdvertisementSearchableQueryParams queryParams)
+        {
+            return query.WhereIf(a => a.Item.Name.Contains(queryParams.Search), !queryParams.Search.IsNullOrWhiteSpace())
+                .ApplyQueryParams(queryParams);
+        }
+
+        public static IQueryable<Advertisement> ApplyQueryParams(this IQueryable<Advertisement> query, AdvertisementQueryParams queryParams)
+        {
+            return query.Include(a => a.Item)
+                    .ThenIncludeIf(i => i.Category, queryParams.IncludeCategory)
+                .Include(a => a.Item)
+                    .ThenIncludeIf(i => i.Images.Take(queryParams.MaxImages ?? int.MaxValue), queryParams.IncludeImages)
+                .IncludeIf(a => a.User, queryParams.IncludeUser)
+                .IncludeIf(a => a.Localization, queryParams.IncludeLocalization)
+                .IncludeIf(a => a.Definition, queryParams.IncludeDefinition);
+        }
+    }
+}
