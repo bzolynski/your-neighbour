d0c1ebc22061498664a0fd651833a751a9618c52
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-11 17:09:50 +0200

    Card elevation fix

--- client-app/src/app/shared/ui/card/card.module.ts
+++ client-app/src/app/shared/ui/card/card.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
+import { ElevatedSectionModule } from '../../directives/elevated-section/elevated-section.module';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, ElevatedSectionModule],
     declarations: [CardComponent],
     exports: [CardComponent],
 })
