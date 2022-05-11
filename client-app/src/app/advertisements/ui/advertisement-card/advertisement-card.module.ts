73f95006080e0976c00bc008c7585a600198612c
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-11 16:57:19 +0200

    Use card component for advertisement card component

--- client-app/src/app/advertisements/ui/advertisement-card/advertisement-card.module.ts
+++ client-app/src/app/advertisements/ui/advertisement-card/advertisement-card.module.ts
@@ -4,9 +4,22 @@ import { AdvertisementCardComponent } from './advertisement-card.component';
 import { SkeletonBoxModule } from 'src/app/shared/ui/skeleton-box/skeleton-box.module';
 import { SkeletonTextModule } from 'src/app/shared/ui/skeleton-text/skeleton-text.module';
 import { MatIconModule } from '@angular/material/icon';
+import { CardModule } from 'src/app/shared/ui/card/card.module';
+import { CardImgModule } from 'src/app/shared/ui/card-img/card-img.module';
+import { CardContentModule } from 'src/app/shared/ui/card-content/card-content.module';
+import { CardFooterModule } from 'src/app/shared/ui/card-footer/card-footer.module';
 
 @NgModule({
-    imports: [CommonModule, SkeletonBoxModule, SkeletonTextModule, MatIconModule],
+    imports: [
+        CommonModule,
+        SkeletonBoxModule,
+        SkeletonTextModule,
+        MatIconModule,
+        CardModule,
+        CardImgModule,
+        CardContentModule,
+        CardFooterModule,
+    ],
     declarations: [AdvertisementCardComponent],
     exports: [AdvertisementCardComponent],
 })
