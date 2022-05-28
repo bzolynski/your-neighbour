983bf1dea11b50bced75944ecb85239fa244a46a
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-28 18:53:25 +0200

    Fix to width

--- client-app/src/app/settings/ui/settings-layout-router/settings-layout-router.component.ts
+++ client-app/src/app/settings/ui/settings-layout-router/settings-layout-router.component.ts
@@ -7,15 +7,21 @@ import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@ang
 })
 export class SettingsLayoutRouterComponent implements OnInit {
     @ViewChild('sideContent', { static: true }) sideContent!: ElementRef<HTMLElement>;
+    @ViewChild('sideContentContainer', { static: true }) sideContentContainer!: ElementRef<HTMLElement>;
     @Input() set expanded(value: boolean | null) {
-        if (value) this.renderer.setStyle(this.sideContent.nativeElement, 'width', this.sideContentWidth);
-        else this.renderer.setStyle(this.sideContent.nativeElement, 'width', 0);
+        if (value) this.#setWidth(this.sideContentWidth);
+        else this.renderer.setStyle(this.sideContentContainer.nativeElement, 'width', 0);
         this.isExpanded = value ?? false;
     }
     @Input() sideContentWidth: string = '600px';
     isExpanded = false;
     constructor(private renderer: Renderer2) {}
     ngOnInit(): void {
-        if (this.isExpanded) this.renderer.setStyle(this.sideContent.nativeElement, 'width', this.sideContentWidth);
+        if (this.isExpanded) this.#setWidth(this.sideContentWidth);
     }
+
+    #setWidth = (width: string) => {
+        this.renderer.setStyle(this.sideContentContainer.nativeElement, 'width', width);
+        this.renderer.setStyle(this.sideContent.nativeElement, 'width', width);
+    };
 }
