24e7288d9b670b4cc7a5ebb245dc561cf7e8b72c
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-28 19:38:37 +0200

    Change width management to observables and subjects

--- client-app/src/app/settings/ui/settings-layout-router/settings-layout-router.component.ts
+++ client-app/src/app/settings/ui/settings-layout-router/settings-layout-router.component.ts
@@ -1,27 +1,31 @@
-import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
+import { BehaviorSubject, combineLatest, iif, of } from 'rxjs';
+import { mergeMap, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-settings-layout-router',
     templateUrl: './settings-layout-router.component.html',
     styleUrls: ['./settings-layout-router.component.scss'],
 })
-export class SettingsLayoutRouterComponent implements OnInit {
+export class SettingsLayoutRouterComponent {
     @ViewChild('sideContent', { static: true }) sideContent!: ElementRef<HTMLElement>;
     @ViewChild('sideContentContainer', { static: true }) sideContentContainer!: ElementRef<HTMLElement>;
     @Input() set expanded(value: boolean | null) {
-        if (value) this.#setWidth(this.sideContentWidth);
-        else this.renderer.setStyle(this.sideContentContainer.nativeElement, 'width', 0);
-        this.isExpanded = value ?? false;
+        this.expanded$.next(value ?? false);
     }
-    @Input() sideContentWidth: string = '600px';
-    isExpanded = false;
+    @Input() set sideContentWidth(value: string) {
+        this.width$.next(value);
+    }
+    expanded$ = new BehaviorSubject<boolean>(false);
+    width$ = new BehaviorSubject<string>('600px');
+    contentWidth$ = combineLatest([this.expanded$.asObservable(), this.width$.asObservable()]).pipe(
+        mergeMap(([expanded, width]) =>
+            iif(
+                () => expanded,
+                of(width).pipe(tap(() => this.renderer.addClass(this.sideContentContainer.nativeElement, 'expanded'))),
+                of('0').pipe(tap(() => this.renderer.removeClass(this.sideContentContainer.nativeElement, 'expanded')))
+            )
+        )
+    );
     constructor(private renderer: Renderer2) {}
-    ngOnInit(): void {
-        if (this.isExpanded) this.#setWidth(this.sideContentWidth);
-    }
-
-    #setWidth = (width: string) => {
-        this.renderer.setStyle(this.sideContentContainer.nativeElement, 'width', width);
-        this.renderer.setStyle(this.sideContent.nativeElement, 'width', width);
-    };
 }
