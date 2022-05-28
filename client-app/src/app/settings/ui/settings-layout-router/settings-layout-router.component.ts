b1228d3dcfcbe480e8879dcddd76f3ce079acb88
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-05-28 18:44:17 +0200

    Add width input prop for settings layout router

--- client-app/src/app/settings/ui/settings-layout-router/settings-layout-router.component.ts
+++ client-app/src/app/settings/ui/settings-layout-router/settings-layout-router.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-settings-layout-router',
     templateUrl: './settings-layout-router.component.html',
     styleUrls: ['./settings-layout-router.component.scss'],
 })
-export class SettingsLayoutRouterComponent {
-    @Input() expanded: boolean | null = false;
+export class SettingsLayoutRouterComponent implements OnInit {
+    @ViewChild('sideContent', { static: true }) sideContent!: ElementRef<HTMLElement>;
+    @Input() set expanded(value: boolean | null) {
+        if (value) this.renderer.setStyle(this.sideContent.nativeElement, 'width', this.sideContentWidth);
+        else this.renderer.setStyle(this.sideContent.nativeElement, 'width', 0);
+        this.isExpanded = value ?? false;
+    }
+    @Input() sideContentWidth: string = '600px';
+    isExpanded = false;
+    constructor(private renderer: Renderer2) {}
+    ngOnInit(): void {
+        if (this.isExpanded) this.renderer.setStyle(this.sideContent.nativeElement, 'width', this.sideContentWidth);
+    }
 }
