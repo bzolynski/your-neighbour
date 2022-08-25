50898e2b9a20125714f4af66530a201cb391cd3b
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-08-25 12:01:21 +0200

    Change skeleton loading

--- client-app/src/app/shared/ui/skeleton-box/skeleton-box.module.ts
+++ client-app/src/app/shared/ui/skeleton-box/skeleton-box.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkeletonBoxComponent } from './skeleton-box.component';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SkeletonModule],
     declarations: [SkeletonBoxComponent],
     exports: [SkeletonBoxComponent],
 })
--- client-app/src/app/shared/ui/skeleton-text/skeleton-text.module.ts
+++ client-app/src/app/shared/ui/skeleton-text/skeleton-text.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkeletonTextComponent } from './skeleton-text.component';
 import { RepeatModule } from '../../directives/repeat/repeat.module';
-
+import { SkeletonModule } from 'primeng/skeleton';
 @NgModule({
-    imports: [CommonModule, RepeatModule],
+    imports: [CommonModule, RepeatModule, SkeletonModule],
     declarations: [SkeletonTextComponent],
     exports: [SkeletonTextComponent],
 })
