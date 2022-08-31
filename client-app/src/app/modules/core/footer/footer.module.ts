6d1216f0b5c0a06db79a861f9d6c601dd00036ca
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2022-08-31 11:43:55 +0200

    Footer update

--- client-app/src/app/modules/core/footer/footer.module.ts
+++ client-app/src/app/modules/core/footer/footer.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
-import { MenubarModule } from 'primeng/menubar';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
-    imports: [CommonModule, MenubarModule],
+    imports: [CommonModule, ToolbarModule],
     declarations: [FooterComponent],
     exports: [FooterComponent],
 })
